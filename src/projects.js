5ecfdfbfd58e33b53db6807acea7feb780388254
Kip Riecken <kip.riecken@gmail.com>
2021-01-11 11:13:34 -0500

    write JSON to be able to iterate over portfolio items

---
+++ src/projects.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        title: "Sam Pederson Photography",
+        description: "",
+        image: "images/SamScreenS.png",
+        url: "https://sampedersonphotography.netlify.app"
+    },
+    {
+        title: "My personal website",
+        description: "",
+        image: "images/MeScreenS.png",
+        url: "https://kipriecken.netlify.app"
+    },
+    {
+        title: "Voting information for 2020 US elections",
+        description: "",
+        image: "images/VotingScreenS.png",
+        url: "https://usvotingapp2020.herokuapp.com/"
+    },
+    {
+        title: "Responsive Advent Calendar",
+        description: "",
+        image: "images/AdventScreenS.png",
+        url: "https://adventcal.netlify.app"
+    },
+]
