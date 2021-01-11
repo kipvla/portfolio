d227374210f2722a227d54f32297b7b9837aa7e8
Kip Riecken <kip.riecken@gmail.com>
2021-01-11 11:12:50 -0500

    store bubble pop-up code for possible later use

---
+++ src/components/Unused.svelte
@@ -0,0 +1,31 @@
+<style>
+        /* a span {
+        color: black;
+        font-size: 16px;
+        background-color: white;
+
+        position: absolute;
+        top: 45%;
+        right: 79%;
+        margin-right: -350px;
+
+        border-style: solid;
+        border-radius: 1rem;
+
+        transform: scale(0);
+        -webkit-transform: scale(0);
+
+        transition: opacity 1s, transform 1.5s;
+        -webkit-transition: opacity 1s, transform 1.5s;
+        transition-delay: 0.5s;
+        -webkit-transition-delay: 0.5s;
+
+        opacity: 0;
+        z-index: 50;
+    }
+    a div:hover ~ span {
+        opacity: 1;
+        transform: scale(1);
+        -webkit-transform: scale(1);
+    } */
+</style>
