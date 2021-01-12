e9aad319cd7325ae97eb64d9f07ddce1009073bc
Kip Riecken <kip.riecken@gmail.com>
2021-01-11 19:08:16 -0500

    add links to sidebar items, fix flexbox layout, increase title size

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -7,32 +7,32 @@
         font-family: 'Source Code Pro', monospace;
 
     }
-    .option {
-        /* flex-grow: 3; */
-        /* height: 50px; */
+    /* .option {
+        flex-grow: 3;
+        height: 50px;
         width: 40%;
         transition: transform 1s, border-radius 1s;
         overflow: hidden;
     }
 
     .option:hover {
-        /* transform: scale(1.05); */
-        /* border-radius: 10px; */
+        transform: scale(1.05);
+        border-radius: 10px;
         transition: transform 1s, border-radius 1s;
-    }
+    } */
   
   .card {
     border: none;
     /* height: 22rem;
     width: 22rem; */
     margin: auto;
-    transition: transform 1s;
+    /* transition: transform 1s; */
   }
   
   .card:hover {
     cursor: pointer;
-    transform: scale(1.02);
-    transition: transform 2s;
+    /* transform: scale(1.02); */
+    /* transition: transform 2s; */
   }
   
   .card div {
@@ -49,7 +49,7 @@
     transition: opacity 1s, background-color 1s;
   }
   
-  .card-img {
+  /* .card-img {
     width: 100%;
     height: 100%;
     object-fit: cover;
@@ -59,6 +59,10 @@
     background: rgb(0, 0, 0, 0.1);
     transform: scale(2);
     transition: transform 1s;
+  } */
+
+  #title {
+      height: 100%;
   }
 
   .sidebar-item {
@@ -74,14 +78,16 @@
 
   #cards {
       align-items: flex-start;
+      overflow: visible;
   }
 
   .sidebar {
       flex-direction: column;
+      justify-content: flex-end;
   }
-  ul {
+  /* ul {
       display: inline;
-  }
+  } */
 
   @media only screen and (max-width: 768px) {
         #all {
@@ -92,12 +98,19 @@
         }
         ul {
             display: flex;
+            justify-content: space-evenly;
+        }
+        .sidebar-item {
+            margin: 0 1rem;
         }
   }
 </style>
 
 <div class="container-fluid d-flex justify-content-between" id="all">
-    <div class="d-flex flex-column justify-content-center order-0">Kip Riecken</div>
+    <div class="d-flex flex-column justify-content-start order-0" id="title">
+        <h1>kip riecken</h1>
+        <div>developer</div>
+    </div>
     <div class="d-flex flex-wrap justify-content-start order-1" id="cards">
         {#each projects as project}
             <div class="card" style="width: 30rem;">
@@ -110,11 +123,11 @@
             </div>
         {/each}
     </div>
-    <div class="d-flex justify-content-center order-0 order-md-2 sidebar">
+    <div class="order-0 order-md-2 sidebar">
         <ul class="list-unstyled">
-            <li class="sidebar-item"><a href="/"><i class="fab fa-github"></i></a> </li>
-            <li class="sidebar-item"><a href="/">Resume</a> </li>
-            <li class="sidebar-item"><a href="/"><i class="fas fa-envelope"></i></a> </li>
+            <li class="sidebar-item"><a href="https://github.com/kipvla"><i class="fab fa-github"></i></a> </li>
+            <li class="sidebar-item"><a href="mailto:kip.riecken@gmail.com"><i class="fas fa-envelope"></i></a> </li>
+            <li class="sidebar-item"><a href="images/TechResume.pdf">resume</a> </li>
         </ul>
     </div>
 </div>
