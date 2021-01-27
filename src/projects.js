c8ae494e258f26a867405adbc36d9224b74239ae
Kip Riecken <kip.riecken@gmail.com>
2021-01-26 19:27:56 -0500

    add my site

--- src/projects.js
+++ src/projects.js
@@ -1,37 +1,42 @@
 export default [
-    {
-        title: "Sam Pederson Photography",
-        description: "",
-        image: "images/SamScreenS.png",
-        url: "https://sampedersenphotography.netlify.app",
-        specs: ["Created with SvelteJS", "Responsive", "Bootstrap formatting"]
-    },
-    // {
-    //     title: "My personal website",
-    //     description: "",
-    //     image: "images/MeScreenS.png",
-    //     url: "https://kipriecken.netlify.app",
-    //     specs: ["Created with SvelteJS", "Responsive", "Bootstrap formatting", "Functioning contact form"]
-    // },
-    {
-        title: "Voting information for 2020 US elections",
-        description: "",
-        image: "images/VotingScreenS.png",
-        url: "https://usvotingapp2020.herokuapp.com/",
-        specs: ["Responsive", "Bootstrap formatting", "Pulls from API"]
-    },
-    {
-        title: "Responsive Advent Calendar",
-        description: "",
-        image: "images/AdventScreenS.png",
-        url: "https://adventcal.netlify.app",
-        specs: ["Created with SvelteJS", "Responsive", "Bootstrap formatting"]
-    },
-    {
-        title: "What should I name my baby?",
-        description: "",
-        image: "images/BabyScreenS.png",
-        url: "https://whatshouldinamemybaby.herokuapp.com/",
-        specs: ["Responsive", "Bootstrap formatting", "Pulls from API"]
-    },
-]
+  {
+    id: 0,
+    title: "Sam Pederson Photography",
+    description: "",
+    image: "images/SamScreenS.png",
+    url: "https://sampedersenphotography.netlify.app",
+    specs: ["Created using SvelteJS", "Responsive", "Bootstrap formatting"],
+  },
+  {
+  id: 1,
+      title: "My personal website",
+      description: "",
+      image: "images/MeScreenS.png",
+      url: "http://me.kipriecken.com",
+      specs: ["Served using Nginx on DigitalOcean", "Golang server", "Postgres database", "Created using SvelteJS", "Responsive", "Bootstrap formatting", "Functioning contact form"]
+  },
+  {
+    id: 2,
+    title: "Voting information for 2020 US elections",
+    description: "",
+    image: "images/VotingScreenS.png",
+    url: "https://usvotingapp2020.herokuapp.com/",
+    specs: ["Responsive", "Bootstrap formatting", "Pulls from API"],
+  },
+  {
+    id: 3,
+    title: "Responsive Advent Calendar",
+    description: "",
+    image: "images/AdventScreenS.png",
+    url: "https://adventcal.netlify.app",
+    specs: ["Created using SvelteJS", "Responsive", "Bootstrap formatting"],
+  },
+  {
+    id: 4,
+    title: "What should I name my baby?",
+    description: "",
+    image: "images/BabyScreenS.png",
+    url: "https://whatshouldinamemybaby.herokuapp.com/",
+    specs: ["Responsive", "Bootstrap formatting", "Pulls from API"],
+  },
+];
